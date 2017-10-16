e528a005e5156919b099e20a58d5c0288b7bff27
Emily Schron <eschron@wellesley.edu>
2017-10-16 16:06:59 -0400

    added unit tests

---
+++ spec/models/truck_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+RSpec.describe 'truck' do
+  scenario 'Adds a truck' do
+    food_truck = Truck.new(name: 'Yummy Food', description: 'The yummiest food', email: 'yummy@food.com')
+    food_truck.save
+
+    expect(Truck.all.length).to eq(1)
+  end
+  scenario 'destroy a truck' do
+    food_truck = Truck.new(name: 'Yummy Food', description: 'The yummiest food', email: 'yummy@food.com')
+    food_truck.save
+    food_truck.destroy
+
+    expect(Truck.all.length).to eq(0)
+  end
+  scenario 'destroy a truck' do
+    food_truck = Truck.new(name: 'Yummy Food', description: 'The yummiest food', email: 'yummy@food.com')
+    food_truck.save
+    food_truck.name = 'Gross Food'
+
+    expect(food_truck.name).to eq('Gross Food')
+  end
+end

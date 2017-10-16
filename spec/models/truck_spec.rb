ab54a21767067432df9f91b7de6d832166a60e20
Emily Schron <eschron@wellesley.edu>
2017-10-16 17:14:49 -0400

    added engative tests to unit tests

--- spec/models/truck_spec.rb
+++ spec/models/truck_spec.rb
@@ -14,4 +14,27 @@ RSpec.describe 'truck' do
       expect(Truck.new(name: 'Yummy Food', description: 'The yummiest food', email: 'yummy@food.com').email).to eq 'yummy@food.com'
     end
   end
+
+  context 'creating truck with missing information' do
+    it 'is missing name' do
+      foodtruck = Truck.new(description: 'The yummiest food', email: 'yummy@food.com')
+      foodtruck.save
+
+      expect(Truck.all.length).to eq(0)
+    end
+
+    it 'is missing description' do
+      foodtruck = Truck.new(name: 'Yummy Food', email: 'yummy@food.com')
+      foodtruck.save
+
+      expect(Truck.all.length).to eq(0)
+    end
+
+    it 'is missing email' do
+      foodtruck = Truck.new(name: 'Yummy Food', description: 'The yummiest food')
+      foodtruck.save
+
+      expect(Truck.all.length).to eq(0)
+    end
+  end
 end

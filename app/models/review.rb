ce3bf234548ae03e5332352d46f7fc84a55c7743
Emily Schron <eschron@wellesley.edu>
2017-10-17 14:12:21 -0400

    Added unit tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   belongs_to :truck
   has_many :votes
 
-  validates :rating, numericality {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
 end

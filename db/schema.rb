e77fc18ba1fbbc4a691fa99807f54937c0887437
Emily Schron <eschron@wellesley.edu>
2017-10-17 11:51:30 -0400

    migrations for category and categorizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016191717) do
+ActiveRecord::Schema.define(version: 20171017142104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "cuisine", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categorizations", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "truck_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categorizations_on_category_id"
+    t.index ["truck_id"], name: "index_categorizations_on_truck_id"
+  end
 
   create_table "trucks", force: :cascade do |t|
     t.string "name", null: false

3b68b1b0ad7cf32365685b0dbdf319f15300ffa2
Emily Schron <eschron@wellesley.edu>
2017-10-19 16:45:22 -0400

    fixed name sensitivy problem

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,12 @@ class SearchController < ApplicationController
     @locations = Location.all
     @categories = Category.all
 
-    if params[:search] != nil && params[:location_id] != "" 
+    if params[:search] != nil && params[:location_id] != ""
       like_keyword = "%#{params[:search]}%"
-      @trucks = Truck.where("name LIKE ?", like_keyword).where(location_id: params[:location_id])
+      @trucks = Truck.where("lower(name) LIKE ?", like_keyword.downcase).where(location_id: params[:location_id])
     elsif params[:search]
       like_keyword = "%#{params[:search]}%"
-      @trucks = Truck.where("name LIKE ?", like_keyword)
+      @trucks = Truck.where("lower(name) LIKE ?", like_keyword.downcase)
     elsif params[:location_id]
       @trucks = Truck.where(location_id: params[:location_id])
     else

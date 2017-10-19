b29b5fc900865db77f1baf5778de707bbc883abb
cbrennan31 <31743841+cbrennan31@users.noreply.github.com>
2017-10-19 13:12:43 -0400

    minor edits in main.js

--- react/src/main.js
+++ react/src/main.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById('app'));
 })

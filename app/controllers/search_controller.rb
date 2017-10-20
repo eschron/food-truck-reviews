a4b6546e920498d2cab759e7ba09369ccb693644
Shannon Paek <shapaek@gmail.com>
2017-10-20 10:33:24 -0400

    finished search logic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,28 @@ class SearchController < ApplicationController
   def index
     @locations = Location.all
     @categories = Category.all
+    if params[:search] != "" && params[:location_id] != "" && params[:category] != nil
 
-    if params[:search] != nil && params[:location_id] != ""
+      like_keyword = "%#{params[:search]}%"
+      trucks1 = Truck.where("lower(name) LIKE ?", like_keyword.downcase).where(location_id: params[:location_id])
+      @trucks = trucks1.joins(categorizations: :category).where(categories: {cuisine: params[:category]})
+    elsif params[:search] != "" && params[:location_id] != ""
       like_keyword = "%#{params[:search]}%"
       @trucks = Truck.where("lower(name) LIKE ?", like_keyword.downcase).where(location_id: params[:location_id])
-    elsif params[:search]
+    elsif params[:location_id] != "" && params[:category] != nil
+      trucks1 = Truck.where(location_id: params[:location_id])
+      @trucks = trucks1.joins(categorizations: :category).where(categories: {cuisine: params[:category]})
+    elsif params[:search] != "" && params[:category] != nil
+      like_keyword = "%#{params[:search]}%"
+      trucks1 = Truck.where("lower(name) LIKE ?", like_keyword.downcase)
+      @trucks = trucks1.joins(categorizations: :category).where(categories: {cuisine: params[:category]})
+    elsif params[:search] != ""
       like_keyword = "%#{params[:search]}%"
       @trucks = Truck.where("lower(name) LIKE ?", like_keyword.downcase)
-    elsif params[:location_id]
+    elsif params[:location_id] != ""
       @trucks = Truck.where(location_id: params[:location_id])
+    elsif params[:category]
+      @trucks = Truck.joins(categorizations: :category).where(categories: {cuisine: params[:category]})
     else
       @trucks = Truck.all
     end

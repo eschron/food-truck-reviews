c51bfd5a90f01b4c07c9992fb6821b10e65e6a6f
jeffery shivers <jefferyshivers@gmail.com>
2017-10-20 10:00:07 -0400

    test presentational components Reviews and ReviewForm

--- react/test/components/ReviewForm.js
+++ react/test/components/ReviewForm.js
@@ -9,4 +9,19 @@ describe('ReviewForm', () => {
     wrapper = mount(<ReviewForm />);
   });
 
+  it('should render a form', () => {
+    expect(wrapper.find('form')).toBePresent();
+  });
+
+  it('should render radio buttons', () => {
+    expect(wrapper.find('input[type="radio"]').length).toEqual(5);
+  });
+
+  it('should render a text area', () => {
+    expect(wrapper.find('textarea')).toBePresent();
+  });
+
+  it('should render a submit button', () => {
+    expect(wrapper.find('input[type="submit"]')).toBePresent();
+  });
 })

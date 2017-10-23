8ae5c04a9c51acc5466e0b9bcaa5a21208e7bf6f
Emily Schron <eschron@wellesley.edu>
2017-10-23 14:13:12 -0400

    resolved merge conflicts in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
+  devise_for :users,
+    skip: [:passwords],
+    controllers: { registrations: "registrations", sessions: "sessions"}
 
-  devise_for :users, :skip => [:passwords], controllers: {
-    registrations: "registrations",
-    sessions: "sessions"}
+  root "home#index"
 
-    root "home#index"
+  namespace :api do
+    resources :reviews, only: [:create]
 
-    resources :users, only: [:show, :index, :destroy]
-    resources :trucks
+    resources :users, only: [:show]
 
-    get "/search", to: "trucks#search"
+    resources :trucks, only: [:show] do
+      resources :reviews, only: [:index]
+    end
   end
+
+  get "/search", to: "trucks#search"
+
+  resources :trucks
+
+  resources :users, only: [:show, :index, :destroy]
+end
